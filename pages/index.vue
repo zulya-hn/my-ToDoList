905d0011ccbe163a86e77d08aeb84735087b8bc0
Zulfiya Hamadysheva <z.hamadysheva@ekf.su>
2021-04-28 21:24:09 +0300

    method fixed

--- pages/index.vue
+++ pages/index.vue
@@ -5,33 +5,29 @@
         <b-col class="task-card border-right-0">
           <h2>Saturday</h2>
           <draggable v-model="saturday" group="list" @start="drag = true" @end="drag = false">
-            <li class="vertical-center" v-for="todo of sat" :key="todo.id">
-              <span :class="{ done: todo.completed }">
-                <input type="checkbox" v-on:change="todo.completed = true" />
-                <strong>
-                  {{ todo.id }}
-                </strong>
-                {{ todo.title }}
+            <li class="vertical-center" v-for="todo of sat.items" :key="todo.id">
+              <span>
+                <input type="checkbox" v-on:change="todo.completed = !todo.completed" :id="todo.id + todo.title" />
+                <label :for="todo.id + todo.title" :class="{ done: todo.completed }">
+                  {{ todo.title }}
+                </label>
               </span>
-              <button class="rm">&times;</button>
+              <button class="rm" @click="removeTodo(todo.id, sat)">&times;</button>
             </li>
           </draggable>
         </b-col>
         <b-col class="task-card">
           <h2>Sunday</h2>
           <draggable v-model="sunday" group="list" @start="drag = true" @end="drag = false">
-            <draggable v-model="saturday" group="list" @start="drag = true" @end="drag = false">
-              <li class="vertical-center" v-for="todo of sun" :key="todo.id">
-                <span :class="{ done: todo.completed }">
-                  <input type="checkbox" v-on:change="todo.completed = true" />
-                  <strong>
-                    {{ todo.id }}
-                  </strong>
+            <li class="vertical-center" v-for="todo of sun.items" :key="todo.id">
+              <span>
+                <input type="checkbox" v-on:change="todo.completed = !todo.completed" :id="todo.id + todo.title" />
+                <label :for="todo.id + todo.title" :class="{ done: todo.completed }">
                   {{ todo.title }}
-                </span>
-                <button class="rm">&times;</button>
-              </li>
-            </draggable>
+                </label>
+              </span>
+              <button class="rm" @click="removeTodo(todo.id, sun)">&times;</button>
+            </li>
           </draggable>
         </b-col>
       </div>
@@ -40,18 +36,15 @@
         <b-col class="task-card">
           <h2>Other days</h2>
           <draggable v-model="otherDays" group="list" @start="drag = true" @end="drag = false">
-            <draggable v-model="saturday" group="list" @start="drag = true" @end="drag = false">
-              <li class="vertical-center" v-for="todo of other" :key="todo.id">
-                <span :class="{ done: todo.completed }">
-                  <input type="checkbox" v-on:change="todo.completed = true" />
-                  <strong>
-                    {{ todo.id }}
-                  </strong>
+            <li class="vertical-center" v-for="todo of other.items" :key="todo.id">
+              <span>
+                <input type="checkbox" v-on:change="todo.completed = !todo.completed" :id="todo.id + todo.title" />
+                <label :for="todo.id + todo.title" :class="{ done: todo.completed }">
                   {{ todo.title }}
-                </span>
-                <button class="rm">&times;</button>
-              </li>
-            </draggable>
+                </label>
+              </span>
+              <button class="rm" @click="removeTodo(todo.id, other)">&times;</button>
+            </li>
           </draggable>
         </b-col>
       </div>
@@ -68,21 +61,32 @@ export default {
   },
   data() {
     return {
-      sat: [
-        { id: 1, title: 'Купить хлеб', completed: false },
-        { id: 2, title: 'Купить масло', completed: false },
-        { id: 3, title: 'Купить молоко', completed: false }
-      ],
-      sun: [
-        { id: 1, title: 'Купить колбасу', completed: false },
-        { id: 2, title: 'Купить коробку', completed: false },
-        { id: 3, title: 'Купить заколку', completed: false }
-      ],
-      other: [
-        { id: 1, title: 'Купить мороженое', completed: false },
-        { id: 2, title: 'Купить лампочку', completed: false },
-        { id: 3, title: 'Купить ризотто', completed: false }
-      ]
+      sat: {
+        items: [
+          { id: 1, title: 'Купить хлеб', completed: false },
+          { id: 2, title: 'Купить масло', completed: false },
+          { id: 3, title: 'Купить молоко', completed: false }
+        ]
+      },
+      sun: {
+        items: [
+          { id: 1, title: 'Купить колбасу', completed: false },
+          { id: 2, title: 'Купить коробку', completed: false },
+          { id: 3, title: 'Купить заколку', completed: false }
+        ]
+      },
+      other: {
+        items: [
+          { id: 1, title: 'Купить мороженое', completed: false },
+          { id: 2, title: 'Купить лампочку', completed: false },
+          { id: 3, title: 'Купить ризотто', completed: false }
+        ]
+      }
+    }
+  },
+  methods: {
+    removeTodo(id, arr) {
+      arr.items = arr.items.filter((t) => t.id !== id)
     }
   }
 }
